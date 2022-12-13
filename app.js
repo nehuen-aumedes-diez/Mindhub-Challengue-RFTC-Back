c6f2a2d8f085aed7752cab76e68e2b6aa31f5ce3
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2022-12-13 00:06:12 -0300

    modelando buzos

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require ('./config/database/database')
+require ('./config/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
