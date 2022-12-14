01c0ae87259afc9bf523620f04724acfdbcea6dd
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-14 02:02:55 -0300

    se define ruta de user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let router = require("express").Router();
+const users = require('../routes/users')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+
+router.use('/api/auth', users)
+
+
+
+
+
+
 
 module.exports = router;
