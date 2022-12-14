51064f2f513822b4c326d49b63a1479d04399cf8
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-14 02:03:28 -0300

    se definen rutas para registrar y verificar usuario

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const { signUp, verify } = require('../controllers/user')
+// const {accountExists} = require('../middlewares/accountExistsSignUp')
+const validator = require('../middlewares/validator')
+const schema = require('../schemas/user')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.post('/signup',validator(schema), signUp)
+router.get('/verify/:code', verify)
+
 
 module.exports = router;
