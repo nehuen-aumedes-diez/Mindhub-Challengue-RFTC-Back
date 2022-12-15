3676c89a5d82957ed6e37874bac89ac3299be565
Nico ojeda <nico_ojeda86@hotmail.com>
2022-12-14 21:42:38 -0300

    se eliminan age, photo y lastname

--- models/User.js
+++ models/User.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
         name: {type: String, required: true},
-        lastName: {type: String, required: true},
-        photo: {type: String, required: true},
-        age: {type: Number, required: true},
+        // lastName: {type: String, required: true},
         email: {type: String, required: true},
         password:{type: String, required: true},
         role: {type: String, required: true},
